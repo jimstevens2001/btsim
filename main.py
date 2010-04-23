19c91ee9245a547151f06904bec749bac4a405d4
James Stevens <jims@fireball.cs.umd.edu>
2010-04-22 20:22:01 -0400

    Added main.py (main event loop)

---
+++ main.py
@@ -0,0 +1,28 @@
+class WorkQueue:
+	def __init__(self):
+		work_queue = []
+		cur_time = 0
+
+	def empty(self):
+		return (work_queue == [])
+
+	def dequeue():
+		pass
+
+	def insert(event):
+		# TODO: Check event format is valid (will need a function for this)
+
+		# Check time
+		if event[2]
+
+# Create the main event queue.
+work_queue = WorkQueue()
+		
+
+# Record format is: [time, type, X, ...] (X and ... can be anything and is determined by the type
+
+# Main queue loop
+while work_queue != []:
+	
+	pass
+
